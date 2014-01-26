5c73a93c77666686888eb03e44eb17c61b903ff9
Fred Prieur <fprieur@gmail.com>
2014-01-26 13:44:59 -0500

    add templates and some html in index

--- firstaidkit.py
+++ firstaidkit.py
@@ -1,8 +1,28 @@
+"""Main module for the royal vic first aid kit website."""
+
+
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.config.update(
+    DEBUG = True,
+)
+
 @app.route("/")
-def index():
-    return "Royal Vic First aid kit"
+def home():
+    """home return the home page."""
+    return render_template("index.html")
+
+
+@app.route("/projects")
+def projects():
+    """projects return projects page."""
+    return "Projects"
+
+
+@app.route("/about")
+def about():
+    """about return the about page."""
+    return "about page"
