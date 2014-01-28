09929a709a3072cebe1734d8a90dc80eb073df1b
Fred Prieur <fprieur@gmail.com>
2014-01-27 20:51:59 -0500

    add about page, modify projects page

--- firstaidkit.py
+++ firstaidkit.py
@@ -6,23 +6,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.config.update(
-    DEBUG = True,
-)
-
 @app.route("/")
 def home():
     """home return the home page."""
     return render_template("index.html")
 
 
-@app.route("/projects")
-def projects():
+@app.route("/projectsv1")
+def projectsv1():
     """projects return projects page."""
-    return render_template("projects.html")
+    return render_template("projectsv1.html")
+
+
+@app.route("/projectsv2")
+def projectsv2():
+    """projects return projects page."""
+    return render_template("projectsv2.html")
 
 
 @app.route("/about")
 def about():
     """about return the about page."""
-    return "about page"
+    return render_template("about.html")
+
+
+@app.route("/donate")
+def donate():
+    """donate return the donate form."""
+    return render_template("donate.html")
