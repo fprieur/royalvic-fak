8247e025f5c84dbffe0a101ea6571466b1695eb5
Fred Prieur <fprieur@gmail.com>
2014-01-28 19:12:59 -0500

    add admin module with peewee

--- firstaidkit.py
+++ firstaidkit.py
@@ -2,9 +2,45 @@
 
 
 import os
+import datetime
 from flask import Flask, render_template
+from flask_peewee.db import Database
+from flask_peewee.auth import Auth
+from flask_peewee.admin import Admin
+from peewee import *
+
+# configure our database
+DATABASE = {
+        'name': 'firstaidkit.db',
+        'engine': 'peewee.SqliteDatabase',
+}
+DEBUG = True
+SECRET_KEY = 'ssshhhh'
 
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+
+# instantiate the db wrapper
+db = Database(app)
+
+# create an Auth object for use with our flask app and database wrapper
+auth = Auth(app, db)
+
+class Note(db.Model):
+    message = TextField()
+    created = DateTimeField(default=datetime.datetime.now)
+
+
+
+admin = Admin(app, auth)
+admin.register(Note)
+
+admin.setup()
+
+# create tables
+auth.User.create_table(fail_silently=True)
+Note.create_table(fail_silently=True)
 
 @app.route("/")
 def home():
